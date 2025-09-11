71868b7af308bf8aac71bfb92abf1ea5e89896e1
ming <mick66145@gmail.com>
2025-09-11 18:00:23 +0800

    feat: jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 // import { ScheduleModule } from '@nestjs/schedule';
 import { configLoads } from './config';
 import { LoggerModule } from './_libs/logger/logger.module';
 import { LoggerMiddleware } from './_libs/logger/logger.middleware';
 import { PrismaModule } from './_libs/prisma/prisma.module';
+import { JwtConfigInterface } from './_libs/auth/jwt-config.interface';
 import { PostModule } from './_examples/post/post.module';
 import { AppController } from './app.controller';
 
@@ -17,6 +19,18 @@ import { AppController } from './app.controller';
     // ScheduleModule.forRoot(), // ref: https://docs.nestjs.com/techniques/task-scheduling
     LoggerModule,
     PrismaModule,
+    JwtModule.registerAsync({
+      global: true,
+      useFactory: (configService: ConfigService) => {
+        const jwt = configService.getOrThrow<JwtConfigInterface>('jwt');
+
+        return {
+          secret: jwt.secret,
+          signOptions: { expiresIn: jwt.expires },
+        };
+      },
+      inject: [ConfigService],
+    }),
     PostModule,
   ],
   controllers: [AppController],
--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,7 @@ import apiKeyConfig from 'src/_libs/auth/api-key.config';
 import loggerConfig from 'src/_libs/logger/logger.config';
 import swaggerConfig from 'src/_libs/app/swagger.config';
 import appProjectConfig from './app-project/app-project.config';
+import jwtConfig from 'src/_libs/auth/jwt.config';
 
 export const configLoads = [
   appConfig,
@@ -10,4 +11,5 @@ export const configLoads = [
   loggerConfig,
   swaggerConfig,
   appProjectConfig,
+  jwtConfig,
 ];
